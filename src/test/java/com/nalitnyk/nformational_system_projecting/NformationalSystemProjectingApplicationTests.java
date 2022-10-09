a83f14b088e4ee0d339fe8d470c99858edb2d998
Prosta4ok_ua <danyavdele@gmail.com>
2022-10-09 18:46:11 +0300

    init

---
+++ src/test/java/com/nalitnyk/nformational_system_projecting/NformationalSystemProjectingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalitnyk.nformational_system_projecting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NformationalSystemProjectingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
