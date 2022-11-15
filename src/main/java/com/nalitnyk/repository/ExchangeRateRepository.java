2c38425ba0da84365afd4a8fdd4a0d867d2014cc
Prosta4ok_ua <danyavdele@gmail.com>
2022-11-15 18:07:24 +0200

    new backup

--- src/main/java/com/nalitnyk/repository/ExchangeRateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nalitnyk.repository;
-
-import com.nalitnyk.model.ExchangeRate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExchangeRateRepository  extends JpaRepository<ExchangeRate, Long> {
-}
