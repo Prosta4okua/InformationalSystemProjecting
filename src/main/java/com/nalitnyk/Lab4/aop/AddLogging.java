a1c4a2fdb66dbdc5614d89cdbed4dc52ff3a8afe
Prosta4ok_ua <danyavdele@gmail.com>
2022-11-16 19:46:56 +0200

    added aop

---
+++ src/main/java/com/nalitnyk/Lab4/aop/AddLogging.java
@@ -0,0 +1,15 @@
+package com.nalitnyk.Lab4.aop;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Transactional
+public @interface AddLogging {
+
+}
