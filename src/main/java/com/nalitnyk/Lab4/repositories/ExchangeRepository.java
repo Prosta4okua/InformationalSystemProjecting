dcf679b338a513def79cab194188dd80a6006e2c
Prosta4ok_ua <danyavdele@gmail.com>
2022-11-23 04:46:42 +0200

    just change

--- src/main/java/com/nalitnyk/Lab4/repositories/ExchangeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nalitnyk.Lab4.repositories;
-
-import com.nalitnyk.Lab4.models.Exchange;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ExchangeRepository extends CrudRepository<Exchange, Integer> {
-}
